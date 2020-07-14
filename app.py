3f3e86d988c4d8cf30ab44fcf3e8a190deabb530
zyq <925355386@qq.com>
2020-07-14 16:08:35 +0800

    app py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootStrap=Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html',url="wwww.baidu.com",list=[1,3,6],dict={'url':"aaa",'name':'zyq'})
+    print(url_for('list',id='baidu.com'))
+    return render_template('test.html',url="wwww.baidu.com",list=[1,3,6],dict={'url':"aaa",'name':'zyq'})
+@app.route('/list/<id>')
+def list():
+    return render_template('test.html',url="wwww.baidu.com")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
